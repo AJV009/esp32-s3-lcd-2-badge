75100974ae603384d682a15c3adef7856c6d5ac1
Alphons Jaimon <jaimonalphons@gmail.com>
2025-11-06 02:29:07 +0530

    it works, playback works

--- esp-demo/mjpeg_badge/MjpegClass.h
+++ esp-demo/mjpeg_badge/MjpegClass.h
@@ -2,7 +2,7 @@
  * MJPEG Parser and Decoder Class
  *
  * This class parses MJPEG format files and decodes individual JPEG frames
- * using the ESP32-S3 hardware JPEG decoder.
+ * using the JPEGDEC software decoder.
  *
  * Based on Arduino_GFX MJPEG examples
  * Adapted for ESP32-S3-LCD-2 Event Badge
@@ -10,55 +10,54 @@
 
 #pragma once
 
-#if defined(ESP32)
 #include <FS.h>
 #include <SD.h>
-#include <ESP32_JPEG_Library.h>
+#include <JPEGDEC.h>
 
-#define READ_BATCH_SIZE 1024  // Read 1KB at a time from SD card
+#define READ_BUFFER_SIZE 1024
 
 class MjpegClass {
 public:
   /*
    * Initialize MJPEG player
    *
-   * @param path Path to MJPEG file on SD card
+   * @param input File stream from SD card
    * @param mjpeg_buf Buffer for reading compressed JPEG data
-   * @param output_buf Buffer for decoded RGB565 frame data
-   * @param output_buf_size Size of output buffer in bytes
+   * @param pfnDraw Callback function for drawing decoded pixels
    * @param useBigEndian Use big endian format for RGB565 output
+   * @param x X position on screen
+   * @param y Y position on screen
+   * @param widthLimit Maximum width
+   * @param heightLimit Maximum height
    * @return true if successful, false otherwise
    */
-  bool setup(const char *path, uint8_t *mjpeg_buf,
-             uint16_t *output_buf, size_t output_buf_size,
-             bool useBigEndian) {
+  bool setup(Stream *input, uint8_t *mjpeg_buf, JPEG_DRAW_CALLBACK *pfnDraw,
+             bool useBigEndian, int x, int y, int widthLimit, int heightLimit) {
 
-    // Open MJPEG file
-    _mjpeg_file = SD.open(path, FILE_READ);
-    if (!_mjpeg_file) {
-      Serial.print("ERROR: Failed to open file: ");
-      Serial.println(path);
-      return false;
+    _input = input;
+    _mjpeg_buf = mjpeg_buf;
+    _pfnDraw = pfnDraw;
+    _useBigEndian = useBigEndian;
+    _x = x;
+    _y = y;
+    _widthLimit = widthLimit;
+    _heightLimit = heightLimit;
+    _inputindex = 0;
+
+    if (!_read_buf) {
+      _read_buf = (uint8_t *)malloc(READ_BUFFER_SIZE);
     }
 
-    // Store buffer pointers
-    _mjpeg_buf = mjpeg_buf;
-    _output_buf = (uint8_t *)output_buf;
-    _output_buf_size = output_buf_size;
-    _useBigEndian = useBigEndian;
-    _read = 0;
-
-    Serial.print("Opened MJPEG file: ");
-    Serial.println(path);
-    Serial.print("File size: ");
-    Serial.print(_mjpeg_file.size());
-    Serial.println(" bytes");
+    if (!_read_buf) {
+      Serial.println("ERROR: Failed to allocate read buffer");
+      return false;
+    }
 
     return true;
   }
 
   /*
-   * Read next JPEG frame from MJPEG file
+   * Read next JPEG frame from MJPEG stream
    *
    * Searches for JPEG SOI marker (FF D8) and EOI marker (FF D9)
    * to extract a complete JPEG frame
@@ -66,188 +65,135 @@ public:
    * @return true if frame found, false if end of file
    */
   bool readMjpegBuf() {
-    if (!_mjpeg_file) {
-      return false;
+    if (_inputindex == 0) {
+      _buf_read = _input->readBytes(_read_buf, READ_BUFFER_SIZE);
+      _inputindex += _buf_read;
     }
-
-    // Handle remaining data from previous read
-    if (_read == 0) {
-      // Buffer is empty, read fresh data
-      _read = _mjpeg_file.read(_mjpeg_buf, READ_BATCH_SIZE);
-      _p = _mjpeg_buf;
-    } else {
-      // Move remaining data to start of buffer
-      memmove(_mjpeg_buf, _p, _read);
-      _p = _mjpeg_buf;
-    }
+    _mjpeg_buf_offset = 0;
+    int i = 0;
+    bool found_FFD8 = false;
 
     // Search for JPEG Start of Image marker (FF D8)
-    bool found_FFD8 = false;
-    while ((_read > 0) && (!found_FFD8)) {
-      while ((_read > 1) && (!found_FFD8)) {
-        --_read;
-        if ((*_p++ == 0xFF) && (*_p == 0xD8)) {  // SOI marker
+    while ((_buf_read > 0) && (!found_FFD8)) {
+      i = 0;
+      while ((i < _buf_read) && (!found_FFD8)) {
+        if ((_read_buf[i] == 0xFF) && (_read_buf[i + 1] == 0xD8)) {
           found_FFD8 = true;
         }
+        ++i;
       }
-
-      if (!found_FFD8) {
-        // Keep searching in new data
-        if (*_p == 0xFF) {
-          // Might be start of SOI, keep FF byte
-          _mjpeg_buf[0] = 0xFF;
-          _read = _mjpeg_file.read(_mjpeg_buf + 1, READ_BATCH_SIZE) + 1;
-        } else {
-          // Read new batch
-          _read = _mjpeg_file.read(_mjpeg_buf, READ_BATCH_SIZE);
-        }
-        _p = _mjpeg_buf;
+      if (found_FFD8) {
+        --i;
+      } else {
+        _buf_read = _input->readBytes(_read_buf, READ_BUFFER_SIZE);
       }
     }
 
-    if (!found_FFD8) {
-      // No more frames found
-      return false;
-    }
-
-    // Rewind pointer to include FF D8
-    --_p;
-    ++_read;
-
-    // Move SOI to start of buffer if needed
-    if (_p > _mjpeg_buf) {
-      memmove(_mjpeg_buf, _p, _read);
-      _p = _mjpeg_buf;
-    }
-
-    // Skip past SOI marker
-    _p += 2;
-    _read -= 2;
-
-    // Ensure we have data to search
-    if (_read == 0) {
-      _read = _mjpeg_file.read(_p, READ_BATCH_SIZE);
-    }
-
-    // Search for JPEG End of Image marker (FF D9)
+    uint8_t *_p = _read_buf + i;
+    _buf_read -= i;
     bool found_FFD9 = false;
-    while ((_read > 0) && (!found_FFD9)) {
-      while ((_read > 1) && (!found_FFD9)) {
-        --_read;
-        if ((*_p++ == 0xFF) && (*_p == 0xD9)) {  // EOI marker
+
+    // Copy JPEG data and search for End of Image marker (FF D9)
+    if (_buf_read > 0) {
+      i = 3;
+      while ((_buf_read > 0) && (!found_FFD9)) {
+        if ((_mjpeg_buf_offset > 0) && (_mjpeg_buf[_mjpeg_buf_offset - 1] == 0xFF) &&
+            (_p[0] == 0xD9)) {
           found_FFD9 = true;
+        } else {
+          while ((i < _buf_read) && (!found_FFD9)) {
+            if ((_p[i] == 0xFF) && (_p[i + 1] == 0xD9)) {
+              found_FFD9 = true;
+              ++i;
+            }
+            ++i;
+          }
         }
+
+        memcpy(_mjpeg_buf + _mjpeg_buf_offset, _p, i);
+        _mjpeg_buf_offset += i;
+        size_t o = _buf_read - i;
+
+        if (o > 0) {
+          memcpy(_read_buf, _p + i, o);
+          _buf_read = _input->readBytes(_read_buf + o, READ_BUFFER_SIZE - o);
+          _p = _read_buf;
+          _inputindex += _buf_read;
+          _buf_read += o;
+        } else {
+          _buf_read = _input->readBytes(_read_buf, READ_BUFFER_SIZE);
+          _p = _read_buf;
+          _inputindex += _buf_read;
+        }
+        i = 0;
       }
 
-      if (!found_FFD9) {
-        // Read more data
-        _read += _mjpeg_file.read(_p + _read, READ_BATCH_SIZE);
+      if (found_FFD9) {
+        return true;
       }
     }
 
-    if (found_FFD9) {
-      // Complete JPEG frame found
-      ++_p;
-      --_read;
-      return true;
-    }
-
-    // Incomplete frame
     return false;
   }
 
   /*
-   * Decode current JPEG frame to RGB565
+   * Decode and draw current JPEG frame
    *
-   * Uses ESP32-S3 hardware JPEG decoder for fast decoding
+   * Uses JPEGDEC library for software decoding
    *
    * @return true if successful, false otherwise
    */
-  bool decodeJpg() {
-    // Configure JPEG decoder
-    jpeg_dec_config_t config = {
-      .output_type = _useBigEndian ? JPEG_RAW_TYPE_RGB565_BE : JPEG_RAW_TYPE_RGB565_LE,
-      .rotate = JPEG_ROTATE_0D,
-    };
+  bool drawJpg() {
+    _remain = _mjpeg_buf_offset;
 
-    // Open decoder
-    _jpeg_dec = jpeg_dec_open(&config);
-    if (!_jpeg_dec) {
-      Serial.println("ERROR: Failed to open JPEG decoder");
+    if (_jpeg.openRAM(_mjpeg_buf, _remain, _pfnDraw) != 1) {
+      Serial.println("ERROR: Failed to open JPEG");
       return false;
     }
 
-    // Allocate I/O callback structure
-    _jpeg_io = (jpeg_dec_io_t *)calloc(1, sizeof(jpeg_dec_io_t));
-    if (!_jpeg_io) {
-      Serial.println("ERROR: Failed to allocate JPEG I/O structure");
-      jpeg_dec_close(_jpeg_dec);
+    if (_scale == -1) {
+      // Calculate scale to fit screen
+      int iMaxMCUs;
+      int w = _jpeg.getWidth();
+      int h = _jpeg.getHeight();
+      float ratio = (float)h / _heightLimit;
+
+      if (ratio <= 1) {
+        _scale = 0;
+        iMaxMCUs = _widthLimit / 16;
+      } else if (ratio <= 2) {
+        _scale = JPEG_SCALE_HALF;
+        iMaxMCUs = _widthLimit / 8;
+        w /= 2;
+        h /= 2;
+      } else if (ratio <= 4) {
+        _scale = JPEG_SCALE_QUARTER;
+        iMaxMCUs = _widthLimit / 4;
+        w /= 4;
+        h /= 4;
+      } else {
+        _scale = JPEG_SCALE_EIGHTH;
+        iMaxMCUs = _widthLimit / 2;
+        w /= 8;
+        h /= 8;
+      }
+
+      _jpeg.setMaxOutputSize(iMaxMCUs);
+      _x = (w > _widthLimit) ? 0 : ((_widthLimit - w) / 2);
+      _y = (_heightLimit - h) / 2;
+    }
+
+    if (_useBigEndian) {
+      _jpeg.setPixelType(RGB565_BIG_ENDIAN);
+    }
+
+    if (_jpeg.decode(_x, _y, _scale) != 1) {
+      Serial.println("ERROR: JPEG decode failed");
+      _jpeg.close();
       return false;
     }
 
-    // Allocate header info structure
-    _out_info = (jpeg_dec_header_info_t *)calloc(1, sizeof(jpeg_dec_header_info_t));
-    if (!_out_info) {
-      Serial.println("ERROR: Failed to allocate JPEG header structure");
-      free(_jpeg_io);
-      jpeg_dec_close(_jpeg_dec);
-      return false;
-    }
-
-    // Set input buffer (JPEG data)
-    _jpeg_io->inbuf = _mjpeg_buf;
-    _jpeg_io->inbuf_len = _p - _mjpeg_buf;
-
-    // Parse JPEG header to get dimensions
-    esp_err_t ret = jpeg_dec_parse_header(_jpeg_dec, _jpeg_io, _out_info);
-    if (ret != ESP_OK) {
-      Serial.print("ERROR: JPEG header parse failed: ");
-      Serial.println(ret);
-      free(_out_info);
-      free(_jpeg_io);
-      jpeg_dec_close(_jpeg_dec);
-      return false;
-    }
-
-    // Store frame dimensions
-    _w = _out_info->width;
-    _h = _out_info->height;
-
-    // Check if output buffer is large enough
-    if ((_w * _h * 2) > _output_buf_size) {
-      Serial.print("ERROR: Frame too large for buffer! ");
-      Serial.print(_w);
-      Serial.print("x");
-      Serial.print(_h);
-      Serial.print(" needs ");
-      Serial.print(_w * _h * 2);
-      Serial.print(" bytes, have ");
-      Serial.println(_output_buf_size);
-      free(_out_info);
-      free(_jpeg_io);
-      jpeg_dec_close(_jpeg_dec);
-      return false;
-    }
-
-    // Set output buffer
-    _jpeg_io->outbuf = _output_buf;
-
-    // Decode JPEG to RGB565
-    ret = jpeg_dec_process(_jpeg_dec, _jpeg_io);
-    if (ret != ESP_OK) {
-      Serial.print("ERROR: JPEG decode failed: ");
-      Serial.println(ret);
-      free(_out_info);
-      free(_jpeg_io);
-      jpeg_dec_close(_jpeg_dec);
-      return false;
-    }
-
-    // Cleanup
-    free(_out_info);
-    free(_jpeg_io);
-    jpeg_dec_close(_jpeg_dec);
-
+    _jpeg.close();
     return true;
   }
 
@@ -255,44 +201,33 @@ public:
    * Get width of decoded frame
    */
   int16_t getWidth() {
-    return _w;
+    return _jpeg.getWidth();
   }
 
   /*
    * Get height of decoded frame
    */
   int16_t getHeight() {
-    return _h;
-  }
-
-  /*
-   * Close MJPEG file
-   */
-  void close() {
-    if (_mjpeg_file) {
-      _mjpeg_file.close();
-    }
+    return _jpeg.getHeight();
   }
 
 private:
-  File _mjpeg_file;              // SD card file handle
-  uint8_t *_mjpeg_buf;           // Buffer for compressed JPEG data
-  uint8_t *_output_buf;          // Buffer for decoded RGB565 data
-  size_t _output_buf_size;       // Size of output buffer
-  bool _useBigEndian;            // RGB565 byte order
+  Stream *_input;
+  uint8_t *_mjpeg_buf;
+  JPEG_DRAW_CALLBACK *_pfnDraw;
+  bool _useBigEndian;
+  int _x;
+  int _y;
+  int _widthLimit;
+  int _heightLimit;
 
-  // JPEG decoder handles
-  jpeg_dec_handle_t *_jpeg_dec;
-  jpeg_dec_io_t *_jpeg_io;
-  jpeg_dec_header_info_t *_out_info;
+  uint8_t *_read_buf = nullptr;
+  int32_t _mjpeg_buf_offset = 0;
 
-  // Frame dimensions
-  int16_t _w = 0;
-  int16_t _h = 0;
+  JPEGDEC _jpeg;
+  int _scale = -1;
 
-  // Parsing state
-  uint8_t *_p;                   // Current read position
-  int32_t _read;                 // Bytes remaining in buffer
+  int32_t _inputindex = 0;
+  int32_t _buf_read;
+  int32_t _remain = 0;
 };
-
-#endif // defined(ESP32)
--- esp-demo/mjpeg_badge/mjpeg_badge.ino
+++ esp-demo/mjpeg_badge/mjpeg_badge.ino
@@ -1,297 +1,190 @@
 /*******************************************************************************
- * ESP32-S3-LCD-2 Event Badge - MJPEG Video Player
+ * ESP32-S3-LCD-2 MJPEG Video Player - Flash Storage Version
  *
- * This sketch plays an MJPEG video file from SD card in an infinite loop
- * on the ESP32-S3-LCD-2 display.
- *
- * Hardware: ESP32-S3-LCD-2 by Waveshare
- * Display: ST7789 2.0" IPS LCD (240x320)
- * Video: output.mjpeg (320x240)
- *
- * Required Libraries:
- * - Arduino_GFX_Library (should be installed)
- * - ESP32_JPEG (included in ESP32 Arduino Core >= 3.0)
+ * Loads video from flash memory (FFat) into PSRAM, then plays in loop.
+ * NO SD CARD NEEDED!
  *
  * Setup:
- * 1. Copy output.mjpeg to SD card root directory
- * 2. Insert SD card into ESP32-S3-LCD-2
- * 3. Upload this sketch
- *
- * Created for OAISYS25 Badge Project
+ * 1. Upload this sketch
+ * 2. Upload data folder with output.mjpeg using upload script
  ******************************************************************************/
 
 #include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
+#include <FFat.h>
 #include <Arduino_GFX_Library.h>
-
 #include "MjpegClass.h"
 
-/*******************************************************************************
- * Pin Definitions - ESP32-S3-LCD-2 Board
- ******************************************************************************/
-// Display pins (SPI)
-#define LCD_SCLK      39
-#define LCD_MOSI      38
-#define LCD_MISO      40
+// Pin Definitions
 #define LCD_CS        45
 #define LCD_DC        42
-#define LCD_RST       -1  // No reset pin
-#define LCD_BL        1   // Backlight
+#define LCD_RST       -1
+#define LCD_BL        1
 
-// SD Card pins (shares SPI with display)
-#define SD_CS         41
-#define SD_SCLK       39  // Shared with LCD
-#define SD_MOSI       38  // Shared with LCD
-#define SD_MISO       40  // Shared with LCD
-
-// Display settings
 #define LCD_WIDTH     240
 #define LCD_HEIGHT    320
-#define LCD_ROTATION  1   // Landscape mode (makes it 320x240)
+#define LCD_ROTATION  1  // Landscape = 320x240
 
-/*******************************************************************************
- * MJPEG Configuration
- ******************************************************************************/
-#define MJPEG_FILENAME    "/output.mjpeg"
-#define MJPEG_BUFFER_SIZE (320 * 240 * 2 / 10)  // Buffer for compressed JPEG data
-#define OUTPUT_BUFFER_SIZE (320 * 240 * 2)      // Buffer for decoded RGB565 frame
+#define MJPEG_FILENAME "/output.mjpeg"
+#define MJPEG_BUFFER_SIZE (320 * 240 * 2 / 4)
 
-/*******************************************************************************
- * Global Objects
- ******************************************************************************/
-// SPI bus for both display and SD card
-SPIClass spi_bus = SPIClass(FSPI);
-
-// Display setup
-Arduino_DataBus *bus = new Arduino_ESP32SPI(
-  LCD_DC, LCD_CS, LCD_SCLK, LCD_MOSI, LCD_MISO, FSPI, true);
-
-Arduino_GFX *gfx = new Arduino_ST7789(
-  bus, LCD_RST, LCD_ROTATION, true /* IPS */, LCD_WIDTH, LCD_HEIGHT);
-
-// MJPEG player
-static MjpegClass mjpeg;
-
-// Buffers
+// Global variables
+uint8_t *mjpeg_file_buffer = NULL;
+size_t mjpeg_file_size = 0;
 uint8_t *mjpeg_buf = nullptr;
-uint16_t *output_buf = nullptr;
 
-/*******************************************************************************
- * Setup Function
- ******************************************************************************/
+// Display objects
+Arduino_DataBus *bus = new Arduino_ESP32SPI(LCD_DC, LCD_CS, 39, 38, 40);
+Arduino_GFX *gfx = new Arduino_ST7789(bus, LCD_RST, LCD_ROTATION, true, LCD_WIDTH, LCD_HEIGHT);
+
+MjpegClass mjpeg;
+
+// Memory stream for reading from PSRAM
+class MemoryStream : public Stream {
+private:
+  uint8_t *buffer;
+  size_t size;
+  size_t position;
+public:
+  MemoryStream(uint8_t *buf, size_t sz) : buffer(buf), size(sz), position(0) {}
+  int available() override { return size - position; }
+  int read() override { return (position < size) ? buffer[position++] : -1; }
+  int peek() override { return (position < size) ? buffer[position] : -1; }
+  size_t readBytes(char *buf, size_t len) override {
+    size_t to_read = min(len, size - position);
+    memcpy(buf, buffer + position, to_read);
+    position += to_read;
+    return to_read;
+  }
+  void reset() { position = 0; }
+  size_t write(uint8_t) override { return 0; }
+  void flush() override {}
+};
+
+MemoryStream *memStream = nullptr;
+
+// JPEG draw callback
+static int jpegDrawCallback(JPEGDRAW *pDraw) {
+  gfx->draw16bitBeRGBBitmap(pDraw->x, pDraw->y, pDraw->pPixels, pDraw->iWidth, pDraw->iHeight);
+  return 1;
+}
+
 void setup() {
   Serial.begin(115200);
-  while (!Serial && millis() < 3000); // Wait for serial or timeout
+  delay(1000);
 
-  Serial.println();
-  Serial.println("===========================================");
-  Serial.println("  ESP32-S3-LCD-2 Event Badge MJPEG Player");
-  Serial.println("===========================================");
-  Serial.println();
+  Serial.println("\n==========================================");
+  Serial.println("ESP32-S3-LCD-2 MJPEG Player (Flash)");
+  Serial.println("==========================================\n");
 
-  // Initialize display
-  Serial.print("Initializing display... ");
+  // Step 1: Initialize display
+  Serial.print("1. Init display... ");
   if (!gfx->begin()) {
     Serial.println("FAILED!");
-    Serial.println("ERROR: Display initialization failed!");
-    while (1) {
-      delay(1000);
-    }
+    while(1) delay(1000);
   }
-  Serial.println("OK");
-
-  // Clear screen and show startup message
+  pinMode(LCD_BL, OUTPUT);
+  digitalWrite(LCD_BL, HIGH);
   gfx->fillScreen(BLACK);
   gfx->setTextColor(WHITE);
   gfx->setTextSize(2);
-  gfx->setCursor(10, 100);
-  gfx->println("Event Badge");
-  gfx->setCursor(10, 130);
-  gfx->println("Loading...");
+  gfx->setCursor(20, 80);
+  gfx->println("ESP32-S3");
+  gfx->setCursor(20, 110);
+  gfx->println("Video Badge");
+  gfx->setCursor(20, 150);
+  gfx->setTextSize(1);
+  gfx->println("Loading from flash...");
+  Serial.println("OK");
 
-  // Turn on backlight
-  pinMode(LCD_BL, OUTPUT);
-  digitalWrite(LCD_BL, HIGH);
-  Serial.println("Display ready (320x240 landscape mode)");
-
-  // Initialize SPI for SD card
-  Serial.print("Initializing SD card... ");
-  spi_bus.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
-
-  if (!SD.begin(SD_CS, spi_bus, 40000000)) {
+  // Step 2: Mount flash filesystem
+  Serial.print("2. Mount flash (FFat)... ");
+  if (!FFat.begin(false, "", 1)) {  // maxOpenFiles = 1 to save RAM
     Serial.println("FAILED!");
-    Serial.println("ERROR: SD Card Mount Failed!");
-    Serial.println("Please check:");
-    Serial.println("  - SD card is inserted");
-    Serial.println("  - SD card is formatted (FAT32)");
-    Serial.println("  - output.mjpeg is in root directory");
-
     gfx->fillScreen(RED);
-    gfx->setCursor(10, 100);
-    gfx->setTextColor(WHITE);
-    gfx->println("SD Card Error!");
-    gfx->setCursor(10, 130);
-    gfx->println("Check card");
-
-    while (1) {
-      delay(1000);
-    }
+    gfx->setCursor(20, 100);
+    gfx->setTextSize(2);
+    gfx->println("Flash Error!");
+    while(1) delay(1000);
   }
   Serial.println("OK");
 
-  // Check if MJPEG file exists
-  Serial.print("Looking for ");
-  Serial.print(MJPEG_FILENAME);
-  Serial.print("... ");
+  // Step 3: Check flash info
+  Serial.printf("   Flash total: %d KB\n", FFat.totalBytes() / 1024);
+  Serial.printf("   Flash used: %d KB\n", FFat.usedBytes() / 1024);
 
-  if (!SD.exists(MJPEG_FILENAME)) {
-    Serial.println("NOT FOUND!");
-    Serial.println("ERROR: output.mjpeg not found on SD card!");
-
+  // Step 4: Open video file from flash
+  Serial.print("3. Open video file... ");
+  File file = FFat.open(MJPEG_FILENAME, "r");
+  if (!file) {
+    Serial.println("FAILED!");
+    Serial.println("   File not found in flash!");
+    Serial.println("   Please upload output.mjpeg using 'Upload Filesystem Image'");
     gfx->fillScreen(RED);
     gfx->setCursor(10, 90);
-    gfx->setTextColor(WHITE);
-    gfx->println("File Not Found!");
+    gfx->setTextSize(2);
+    gfx->println("File Error!");
     gfx->setCursor(10, 120);
     gfx->setTextSize(1);
-    gfx->println("Please copy output.mjpeg");
-    gfx->setCursor(10, 140);
-    gfx->println("to SD card root directory");
-
-    while (1) {
-      delay(1000);
-    }
+    gfx->println("Upload data folder");
+    gfx->println("with output.mjpeg");
+    while(1) delay(1000);
   }
+  mjpeg_file_size = file.size();
+  Serial.printf("OK (%d bytes = %d KB)\n", mjpeg_file_size, mjpeg_file_size/1024);
 
-  File mjpegFile = SD.open(MJPEG_FILENAME);
-  size_t fileSize = mjpegFile.size();
-  mjpegFile.close();
-  Serial.println("FOUND");
-  Serial.print("File size: ");
-  Serial.print(fileSize);
-  Serial.println(" bytes");
-
-  // Allocate buffers
-  Serial.print("Allocating buffers... ");
-
-  // Use aligned allocation for better performance with SIMD
-  mjpeg_buf = (uint8_t *)heap_caps_aligned_alloc(16, MJPEG_BUFFER_SIZE, MALLOC_CAP_8BIT);
-  if (!mjpeg_buf) {
-    Serial.println("FAILED!");
-    Serial.println("ERROR: Failed to allocate MJPEG buffer!");
+  // Step 5: Allocate PSRAM
+  Serial.print("4. Allocate PSRAM... ");
+  mjpeg_file_buffer = (uint8_t*)ps_malloc(mjpeg_file_size);
+  if (!mjpeg_file_buffer) {
+    Serial.println("FAILED! Not enough PSRAM");
     gfx->fillScreen(RED);
-    gfx->setCursor(10, 100);
+    gfx->setCursor(20, 100);
+    gfx->setTextSize(2);
     gfx->println("Memory Error!");
-    while (1) delay(1000);
+    file.close();
+    while(1) delay(1000);
   }
+  Serial.printf("OK (%d KB allocated)\n", mjpeg_file_size/1024);
 
-  output_buf = (uint16_t *)heap_caps_aligned_alloc(16, OUTPUT_BUFFER_SIZE, MALLOC_CAP_8BIT);
-  if (!output_buf) {
-    Serial.println("FAILED!");
-    Serial.println("ERROR: Failed to allocate output buffer!");
-    free(mjpeg_buf);
-    gfx->fillScreen(RED);
-    gfx->setCursor(10, 100);
-    gfx->println("Memory Error!");
-    while (1) delay(1000);
-  }
+  // Step 6: Load file to PSRAM
+  Serial.print("5. Load to PSRAM... ");
+  gfx->setCursor(20, 180);
+  gfx->println("Reading from flash...");
 
-  Serial.println("OK");
-  Serial.print("MJPEG buffer: ");
-  Serial.print(MJPEG_BUFFER_SIZE);
-  Serial.println(" bytes");
-  Serial.print("Output buffer: ");
-  Serial.print(OUTPUT_BUFFER_SIZE);
-  Serial.println(" bytes");
+  size_t bytes_read = file.read(mjpeg_file_buffer, mjpeg_file_size);
+  file.close();
 
-  // Setup MJPEG player
-  Serial.print("Initializing MJPEG player... ");
-  if (!mjpeg.setup(MJPEG_FILENAME, mjpeg_buf, output_buf, OUTPUT_BUFFER_SIZE, true)) {
-    Serial.println("FAILED!");
-    Serial.println("ERROR: MJPEG setup failed!");
-    gfx->fillScreen(RED);
-    gfx->setCursor(10, 100);
-    gfx->println("Player Error!");
-    while (1) delay(1000);
+  if (bytes_read != mjpeg_file_size) {
+    Serial.printf("FAILED! Read %d/%d bytes\n", bytes_read, mjpeg_file_size);
+    free(mjpeg_file_buffer);
+    while(1) delay(1000);
   }
   Serial.println("OK");
 
-  // All ready!
-  Serial.println();
-  Serial.println("===========================================");
-  Serial.println("  System ready! Starting playback...");
-  Serial.println("===========================================");
-  Serial.println();
+  Serial.println("\n========================================");
+  Serial.println("SUCCESS! Video loaded from flash");
+  Serial.println("========================================\n");
 
-  delay(1000);  // Brief pause before starting
+  // Step 7: Setup MJPEG player
+  Serial.print("6. Setup player... ");
+  memStream = new MemoryStream(mjpeg_file_buffer, mjpeg_file_size);
+  mjpeg_buf = (uint8_t*)malloc(MJPEG_BUFFER_SIZE);
+
+  if (!mjpeg_buf || !mjpeg.setup(memStream, mjpeg_buf, jpegDrawCallback, true, 0, 0, 320, 240)) {
+    Serial.println("FAILED!");
+    while(1) delay(1000);
+  }
+  Serial.println("OK");
+
+  Serial.println("\nStarting playback...\n");
   gfx->fillScreen(BLACK);
 }
 
-/*******************************************************************************
- * Main Loop - Plays video continuously
- ******************************************************************************/
 void loop() {
-  static unsigned long loop_count = 0;
-  static unsigned long frame_count = 0;
-  static unsigned long loop_start_ms = 0;
-
-  loop_start_ms = millis();
-  loop_count++;
-
-  Serial.print("Loop #");
-  Serial.println(loop_count);
-
-  // Play through entire video
-  while (mjpeg.readMjpegBuf()) {
-    // Decode JPEG frame
-    if (mjpeg.decodeJpg()) {
-      // Get decoded frame dimensions
-      int16_t w = mjpeg.getWidth();
-      int16_t h = mjpeg.getHeight();
-
-      // Center on screen (should be perfect match: 320x240)
-      int16_t x = (gfx->width() - w) / 2;
-      int16_t y = (gfx->height() - h) / 2;
-
-      // Display frame (using Big Endian RGB565)
-      gfx->draw16bitBeRGBBitmap(x, y, output_buf, w, h);
-
-      frame_count++;
-    }
-
-    // Small delay to control frame rate (optional)
-    // delay(1);  // Uncomment if playback is too fast
+  if (mjpeg.readMjpegBuf()) {
+    mjpeg.drawJpg();
+  } else {
+    // Loop back to start
+    memStream->reset();
   }
-
-  // Video finished, loop back to start
-  unsigned long loop_duration = millis() - loop_start_ms;
-
-  Serial.print("  Frames displayed: ");
-  Serial.println(frame_count);
-  Serial.print("  Loop duration: ");
-  Serial.print(loop_duration);
-  Serial.println(" ms");
-  Serial.print("  Average FPS: ");
-  Serial.println(1000.0 * frame_count / loop_duration, 1);
-  Serial.println("  Restarting...");
-  Serial.println();
-
-  // Close and reopen file for next loop
-  mjpeg.close();
-
-  // Small delay before restarting (optional)
-  delay(100);
-
-  // Restart MJPEG player
-  if (!mjpeg.setup(MJPEG_FILENAME, mjpeg_buf, output_buf, OUTPUT_BUFFER_SIZE, true)) {
-    Serial.println("ERROR: Failed to restart MJPEG player!");
-    gfx->fillScreen(RED);
-    gfx->setCursor(10, 100);
-    gfx->setTextColor(WHITE);
-    gfx->println("Restart Error!");
-    while (1) delay(1000);
-  }
-
-  frame_count = 0;  // Reset frame counter for next loop
 }
